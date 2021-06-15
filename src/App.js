94111274a9baf5feec0474be5124e2104f8ac65a
mmkobylin <mmkobylin@gmail.com>
2021-06-15 20:09:11 +0100

    use state and on change added to capture text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  
+  const [ noteInput, setNoteInput ] = useState(' ');
 
   const addNote = () => {}
+
   return (
     <div className="App">
       <h1>
         Sticky Notes
       </h1>
         <form onSubmit = { addNote } className="note-form">
-          <textarea placeholder = "Create a new note" ></textarea>
+          <textarea 
+            value = { noteInput }
+            placeholder = "Create a new note"
+            onChange = { (e) => { setNoteInput ( e.target.value ) } }
+          >
+          </textarea>
           <button>Add!</button>
         </form>
     </div>
