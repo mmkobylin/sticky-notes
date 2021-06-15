76751d3e65d6dfefb6d2fd299c6a45a082d8fcdb
mmkobylin <mmkobylin@gmail.com>
2021-06-15 19:31:03 +0100

    basic content created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const addNote = () => {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Sticky Notes
+      </h1>
+        <form onSubmit = { addNote } className="note-form">
+          <textarea placeholder = "Create a new note" ></textarea>
+          <button>Add!</button>
+        </form>
     </div>
   );
 }
